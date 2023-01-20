cf3a0a01563a7f092f77e620edfa1307e538ba32
Dkingofcode <doladepo128@gmail.com>
2023-01-20 23:34:44 +0000

    Created the markup and styled the layout

---
+++ Dictionary-App/index.js
@@ -0,0 +1,19 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
